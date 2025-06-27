d3a326857e70d438e48ce067627c61ad1cfe103d
Christian <christian.siegrist@open-circle.ch>
2025-06-27 10:00:06 +0200

    added inputArray split into 3 Arrays (Numbers, Operator, Number) git push
    exit
    ''

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let no1 = 4;
-let no2 = 2;
-let op = "/";
+
+
+let inputArray = []
+let opIndex = 0;
 
 // expression
-function operate(no1, no2, op) {
+function result(no1, no2, op) {
     if (op === "+") return add(no1, no2);
     if (op === "-") return subtract(no1, no2);
     if (op === "*") return multiply(no1, no2);
@@ -29,6 +30,67 @@ function divide(no1, no2) {
 
 //button listener
 const buttonsList = document.querySelectorAll(".button")
-const buttons = Array.from(buttonsList)
-console.log(buttons)
-console.log(operate(no1, no2, op))
+buttonsList.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const input = event.target.value;
+        //check for Number
+        if(!isNaN(input)){
+            inputArray.push(Number(input));
+        } else {
+            inputArray.push(input);
+        }
+        
+        document.querySelector(".output").innerText = inputArray.join("")
+        opIndex = inputArray.indexOf(6)
+    })
+});
+
+function getNoOne(inputArray) {     // Get first Numbers
+    const noOne = [];
+
+    
+    while (typeof inputArray[0] === "number") {
+        noOne.push(inputArray.shift());
+    }
+
+    console.log("noOne:", noOne);
+    getOp(inputArray);
+    return noOne;
+}
+
+function getOp(inputArray) {           // Get Operator
+    const op = [];
+
+    
+    if (typeof inputArray[0] !== "number") {
+        op.push(inputArray.shift());
+        console.log("op:", op);
+        getNoTwo(inputArray);
+    }
+}
+
+function getNoTwo(inputArray) {         // Get second Number
+    const noTwo = [];  
+
+    while (typeof inputArray[0] === "number") {
+        noTwo.push(inputArray.shift());
+    }
+
+    console.log("noTwo:", noTwo);
+
+}
+
+
+
+// const buttons = Array.from(buttonsList)
+
+
+
+
+
+// console.log(result(no1, no2, op))  
+// 
+
+  //      const opIndex = inputArray.findIndex((el) => typeof el === "string" && ["+", "-", "*", "/"].includes(el));
+  //      console.log("Erster Operator an Index:", opIndex);
+  //  });
